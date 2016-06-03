a8be70be76b24506008096b19cdf66170e7849ff
Mario Ojeda <marioaojeda@gmail.com>
2016-06-03 20:31:32 +0000

    npm init + cambiando la versión de express y usando listen()

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,7 @@
-var aloha = 'Aloha variables';
-console.log(aloha);
+var express = require("express");
+var app = express();
+var port = process.env.PORT;
+
+app.listen(port, function(err) {
+    console.log('The server is running on port: ' + port);
+});
