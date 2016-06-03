7e768bce4d40483a284a623c45ba813288c544a1
Mario Ojeda <marioaojeda@gmail.com>
2016-06-03 20:46:35 +0000

    Routing con app.get()

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -2,6 +2,14 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT;
 
+app.get('/', function(request, response) {
+    response.send("Hola!");    
+});
+
+app.get('/routing', function(request, response) {
+    response.send("Hola! (En routing)");    
+});
+
 app.listen(port, function(err) {
     console.log('The server is running on port: ' + port);
 });
