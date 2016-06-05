d01f89af6b5349434f6b1d2b1b90b75652ea6c38
Mario Ojeda <marioaojeda@gmail.com>
2016-06-05 21:56:30 +0000

    Usando wiredep para manejar los script tags de bootstrap y jquery en index.html

--- nodeproject/gulpfile.js
+++ nodeproject/gulpfile.js
@@ -6,3 +6,16 @@ gulp.task('style', function(){
     return gulp.src(jsfiles)
         .pipe(jshint());
 });
+
+gulp.task('inject', function(){
+    var wiredep = require("wiredep").stream;
+    var options = {
+        directory: './bower_components',
+        bowerJson: require('./bower.json'),
+        ignorePath: '../../bower_components'
+    };
+   
+   return gulp.src('./src/views/*.html')
+    .pipe(wiredep(options))
+    .pipe(gulp.dest('./src/views'));
+});
