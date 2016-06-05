dc45ed9ab62a2a4711bc840237b951043afb9b82
Mario Ojeda <marioaojeda@gmail.com>
2016-06-05 22:32:28 +0000

    Inyectando custom css y js como dependencias usando gulp-inject

--- nodeproject/gulpfile.js
+++ nodeproject/gulpfile.js
@@ -1,6 +1,6 @@
-var gulp = require('gulp');
+var gulp    = require('gulp');
 var jsfiles = ['*.js', 'src/**/*.js'];
-var jshint = require('gulp-jshint');
+var jshint  = require('gulp-jshint');
 
 gulp.task('style', function(){
     return gulp.src(jsfiles)
@@ -8,14 +8,32 @@ gulp.task('style', function(){
 });
 
 gulp.task('inject', function(){
-    var wiredep = require("wiredep").stream;
-    var options = {
+    var wiredep         = require("wiredep").stream;
+    var wiredepOptions  = {
         directory: './bower_components',
         bowerJson: require('./bower.json'),
         ignorePath: '../../bower_components'
     };
-   
+    
+    var gulpInject          = require("gulp-inject");
+    var gulpInjectsrc       = gulp.src(['./public/css/*.css', './public/js/*.js']);
+    var gulpInjectOptions   = {
+         ignorePath: '/public/'
+     }
+       
    return gulp.src('./src/views/*.html')
-    .pipe(wiredep(options))
-    .pipe(gulp.dest('./src/views'));
+    .pipe(
+        wiredep(
+            wiredepOptions
+        )
+    )
+    .pipe(
+        gulpInject(
+            gulpInjectsrc, 
+            gulpInjectOptions
+        )
+    )
+    .pipe(
+        gulp.dest('./src/views')
+    );
 });
