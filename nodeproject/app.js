a7cc2fb8a1fefe28c9fab102c07dc77293e491c5
Mario Ojeda <marioaojeda@gmail.com>
2016-06-30 02:43:35 +0000

    Creando nuevo template para eventos e iterando sobre datos de eventos

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -10,9 +10,88 @@ app.use(express.static('bower_components'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+var eventData = [
+        {
+            name: 'Event 1',
+            description: 'The first event',
+            date: '2016.01.01',
+            time: '1:00 PM',
+            duration: '1 Hour',
+            location: {
+                streetAddr: '100 Main St.',
+                city: 'Los Angeles',
+                state: 'CA',
+                zip: '87885',
+                lon: 0,
+                lat: 0
+            },
+            capacity: 100
+        },
+        {
+            name: 'Event 2',
+            description: 'The second event',
+            date: '2016.01.01',
+            time: '1:00 PM',
+            duration: '1 Hour',
+            location: {
+                streetAddr: '100 Main St.',
+                city: 'Los Angeles',
+                state: 'CA',
+                zip: '87885',
+                lon: 0,
+                lat: 0
+            },
+            capacity: 100
+        },
+        {
+            name: 'Event 3',
+            description: 'The third event',
+            date: '2016.01.01',
+            time: '1:00 PM',
+            duration: '1 Hour',
+            location: {
+                streetAddr: '100 Main St.',
+                city: 'Los Angeles',
+                state: 'CA',
+                zip: '87885',
+                lon: 0,
+                lat: 0
+            },
+            capacity: 100
+        },
+        {
+            name: 'Event 4',
+            description: 'The fourth event',
+            date: '2016.01.01',
+            time: '1:00 PM',
+            duration: '1 Hour',
+            location: {
+                streetAddr: '100 Main St.',
+                city: 'Los Angeles',
+                state: 'CA',
+                zip: '87885',
+                lon: 0,
+                lat: 0
+            },
+            capacity: 100
+        }
+    ];
+
 eventRouter.route('/')
     .get(function(req, res) {
-        res.send('Hola events!');
+        res.render('events',
+            {
+            navigation: [
+                { Link: 'services', Text: 'Services'},
+                { Link: 'portfolio', Text: 'Portfolio'},
+                { Link: 'about', Text: 'About'},
+                { Link: 'team', Text: 'Team'},
+                { Link: 'contact', Text: 'Contact'},
+                { Link: 'events', Text: 'Events'}
+                ],
+            events: eventData
+            }
+        );
     });
 
 eventRouter.route('/event')
