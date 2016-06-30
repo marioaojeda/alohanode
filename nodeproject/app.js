e39d2015afa0842ed7527d91d4d0fa445245db7f
Mario Ojeda <marioaojeda@gmail.com>
2016-06-30 01:26:53 +0000

    Instanciando express.Router() y agregando dos rutas para eventos. Cleanup

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT;
+var eventRouter = express.Router();
 
 app.use(express.static('public'));
 //app.use(express.static('src/views'));
@@ -9,12 +10,30 @@ app.use(express.static('bower_components'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+eventRouter.route('/')
+    .get(function(req, res) {
+        res.send('Hola events!');
+    });
+
+eventRouter.route('/event')
+    .get(function(req, res) {
+        res.send('Hola single event!');
+    });
+
+app.use('/events', eventRouter);
+
 app.get('/', function(request, response) {
     response.render(
         'index', 
         { 
-            list: ['Item1', 'Item2', 'Item3'],
-            navigation: ['services', 'portfolio', 'about', 'team', 'contact']
+            navigation: [
+                { Link: 'services', Text: 'Services'},
+                { Link: 'portfolio', Text: 'Portfolio'},
+                { Link: 'about', Text: 'About'},
+                { Link: 'team', Text: 'Team'},
+                { Link: 'contact', Text: 'Contact'},
+                { Link: 'events', Text: 'Events'}
+            ]
         }
     );
 });
