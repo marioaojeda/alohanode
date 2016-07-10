bb0b94c6568b090db14713abe115e81e751195a5
Mario Ojeda <marioaojeda@gmail.com>
2016-07-10 16:55:44 +0000

    Agrega los datos iniciales

--- nodeproject/src/routes/dbRoutes.js
+++ nodeproject/src/routes/dbRoutes.js
@@ -1,9 +1,84 @@
 var express = require('express');
 var dbRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+var eventData = [
+        {
+            name: 'Event 1',
+            description: 'The first event',
+            date: '2016.01.01',
+            time: '1:00 PM',
+            duration: '1 Hour',
+            location: {
+                streetAddr: '100 Main St.',
+                city: 'Los Angeles',
+                state: 'CA',
+                zip: '87885',
+                lon: 0,
+                lat: 0
+            },
+            capacity: 100
+        },
+        {
+            name: 'Event 2',
+            description: 'The second event',
+            date: '2016.01.01',
+            time: '1:00 PM',
+            duration: '1 Hour',
+            location: {
+                streetAddr: '100 Main St.',
+                city: 'Los Angeles',
+                state: 'CA',
+                zip: '87885',
+                lon: 0,
+                lat: 0
+            },
+            capacity: 100
+        },
+        {
+            name: 'Event 3',
+            description: 'The third event',
+            date: '2016.01.01',
+            time: '1:00 PM',
+            duration: '1 Hour',
+            location: {
+                streetAddr: '100 Main St.',
+                city: 'Los Angeles',
+                state: 'CA',
+                zip: '87885',
+                lon: 0,
+                lat: 0
+            },
+            capacity: 100
+        },
+        {
+            name: 'Event 4',
+            description: 'The fourth event',
+            date: '2016.01.01',
+            time: '1:00 PM',
+            duration: '1 Hour',
+            location: {
+                streetAddr: '100 Main St.',
+                city: 'Los Angeles',
+                state: 'CA',
+                zip: '87885',
+                lon: 0,
+                lat: 0
+            },
+            capacity: 100
+        }
+    ];
 
 dbRouter.route('/AddEventData')
     .get( function(req, res) {
-        res.send("Success!");
+        var url = 'mongodb://localhost:27017/eventsApp';
+        mongodb.connect(url, function(err, db) {
+           var collection = db.collection('events');
+
+           collection.insertMany(eventData, function(err, results){
+               res.send(results);
+               db.close();
+           });
+        });
     });
 
 module.exports = dbRouter;
