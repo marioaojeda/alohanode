50b11b9352d32f96b952bca5c2f21ddd7c4ad98b
Mario Ojeda <marioaojeda@gmail.com>
2016-07-10 16:39:58 +0000

    Agrega nueva ruta para db

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT;
 var eventRouter = require('./src/routes/eventRoutes');
+var dbRouter = require('./src/routes/dbRoutes');
 
 app.use(express.static('public'));
 //app.use(express.static('src/views'));
@@ -11,6 +12,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/events', eventRouter);
+app.use('/db', dbRouter);
 
 app.get('/', function(request, response) {
     response.render(
