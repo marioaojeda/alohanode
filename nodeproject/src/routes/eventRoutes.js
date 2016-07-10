0e5cb8e57efffa4a7797a2a16e9738e6de07d63e
Mario Ojeda <marioaojeda@gmail.com>
2016-07-10 17:05:49 +0000

    Conecta la bd para /events

--- nodeproject/src/routes/eventRoutes.js
+++ nodeproject/src/routes/eventRoutes.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var eventRouter  = express.Router();
+var mongodb = require('mongodb').MongoClient;
 
 eventRouter.route('/')
     .get(function(req, res) {
-        res.render('events',
-            {
-            navigation: [
-                { Link: 'services', Text: 'Services'},
-                { Link: 'portfolio', Text: 'Portfolio'},
-                { Link: 'about', Text: 'About'},
-                { Link: 'team', Text: 'Team'},
-                { Link: 'contact', Text: 'Contact'},
-                { Link: 'events', Text: 'Events'}
-                ],
-            events: eventData
-            }
-        );
+        var url = 'mongodb://localhost:27017/eventsApp';
+        mongodb.connect(url, function(err, db) {
+            var collection = db.collection('events');
+            collection.find({}).toArray(function (err, results) {
+
+                res.render('events',
+                    {
+                    navigation: [
+                        { Link: 'services', Text: 'Services'},
+                        { Link: 'portfolio', Text: 'Portfolio'},
+                        { Link: 'about', Text: 'About'},
+                        { Link: 'team', Text: 'Team'},
+                        { Link: 'contact', Text: 'Contact'},
+                        { Link: 'events', Text: 'Events'}
+                        ],
+                    events: results
+                    }
+                );
+            });
+        });
     });
 
 eventRouter.route('/:id')
